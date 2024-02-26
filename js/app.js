f00a26dcb01f1ac49c59b22e779d9e91da511f69
ahmad <ahmadabbdi15@gmail.com>
2024-02-26 22:54:25 +0330

    Complete calculator

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@ function display(value) {
   CurrentResult += value;
   result.value = CurrentResult;
 }
+function evl() {
+    try {
+      result.value = math.evaluate(CurrentResult);
+      CurrentResult = result.value;
+    } catch (error) {
+      result.value = "Error";
+      CurrentResult = "";
+    }
+  }
+  function del(){
+      let del=document.getElementById("inputtext")
+      del.value=del.value.slice(0,-1)
+  }
+  function clear() {
+      let inp = document.getElementById("inputtext");
+      inp.value = ""
+  }
+  let clearbtn=document.getElementById("clear")
+  clearbtn.addEventListener("click",()=>{
+      result.value=""
+      CurrentResult=""
+  })
